13f5b518cf6b30dd82ec6c8d15a5bb88fbc16a14
Patrick <118749827+Patrix3tv@users.noreply.github.com>
2024-04-24 15:19:48 +0200

    Update navigation.php

--- navigation.php
+++ navigation.php
@@ -4,7 +4,12 @@ if (isset($_GET['rider'])) $Reiter=($_GET['rider']);
 ?>
 <ul>
   <li><a href="index.php?show=home" <?php if ($rider==0) echo"class='you-are-here'";?>>Home</a></li>
-  <li><a href="index.php?show=discordbot&rider=1" <?php if ($rider==1) echo" class='you-are-here'";?>>Discord Bot</a></li>
+  <li><a <?php if ($rider==1) echo" class='you-are-here'";?>>Discord Bots</a>
+    <ul class="level2">
+      <a href="index.php?show=discordbot1&rider=1">Gameserver Bot</a>
+      <a href="index.php?show=discordbot2&rider=1">A possible new bot for Schrott Games Stats</a>
+    </ul>
+  </li>
   <li><a href="index.php?show=kontakt&rider=2"<?php if ($rider==2) echo" class='you-are-here'";?>>Contact</a></li>  
   <li><a href="index.php?show=impressum&rider=3"<?php if ($rider==3) echo" class='you-are-here'";?>>Impressum</a></li>
 </ul>
