daf3b2033c4fb6df827a1c61893bc51de7887b92
Patrick <118749827+Patrix3tv@users.noreply.github.com>
2024-04-24 11:21:38 +0200

    Aktualisieren von

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+if (isset($_GET['show'])) $show = $_GET['show'];
+else $show = home;
+?>
+
+<head>
+<meta charset="utf-8">
+<title>Patrix Discord Bot Overview</title>
+<meta description="">
+<link href="central.css">
+<style>
+a {
+text-decoration: none;
+color: black;
+}
+</style>
+</head>
+
+<body>
+
+</body>
+</html>
